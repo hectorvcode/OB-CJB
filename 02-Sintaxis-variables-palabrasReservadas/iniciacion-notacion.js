c4423c3199c07d2beb936681d1581045ec77cb12
HectorV <hectorfabiovasquez@gmail.com>
2022-09-20 08:56:05 -0500

    Added Listas, Objetos y Fechas en JS

---
+++ 02-Sintaxis-variables-palabrasReservadas/iniciacion-notacion.js
@@ -0,0 +1,26 @@
+//Notacion
+// ; -> buena práctica que delimita el final de una línea pero actualmente no es obligatorio
+const b = 4;
+b + 4
+
+// . -> se utiliza en los objetos para acceder a los atributos
+//movil.tamaño
+
+// [] -> se utilizan para las listas, arreglos o arrays
+const arr = [1, 2, 3, 4]
+console.log(arr[2]);
+
+// () -> se utilizan para las funciones
+function suma(a, b){
+    //Se escribe la función
+}
+
+// {} -> se usa para objetos, funciones y estructuras de control
+const movil = {
+    anchura: 5,
+    altura: 10
+}
+
+if(true){
+    //hace esto
+}
