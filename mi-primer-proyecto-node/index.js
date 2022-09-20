695bd0785a60667f664d154ffa9835b5fd5e9895
HectorV <hectorfabiovasquez@gmail.com>
2022-09-19 21:50:27 -0500

    Added Ejecución y Comentarios Section

---
+++ mi-primer-proyecto-node/index.js
@@ -0,0 +1,14 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*
+Este es un comentario multilínea
+Esta es la parte intermedia de nuestro porycto
+En esta parte realizamos los chequeos necesarios
+
+
+Por ultimo vamos a la ultima linea de saludo
+*/
+
+// Este va a ser el mensaje que se envía para despegar
+console.log("Adios");
