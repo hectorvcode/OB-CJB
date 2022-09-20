5fd1341b779b1d9859371f9828a48a7a612aa200
HectorV <hectorfabiovasquez@gmail.com>
2022-09-20 18:42:35 -0500

    Added Ejercicio 1 Section 2

---
+++ 02-Sintaxis-variables-palabrasReservadas/Ejercicio1-Seccion2.js
@@ -0,0 +1,15 @@
+const nombre = "Fabio Lopez";
+const edad = 42;
+const eresDesarrollador = true;
+const fechaNacimiento = Date("January 23 1980");
+const libroFavorito = {
+    titulo : "the never ending story",
+    autor: "Michael Ende",
+    fecha: Date("September 1 1979"),
+    url: "rebrand.ly/be4edb",
+}
+
+
+const lista = [nombre, edad, eresDesarrollador, fechaNacimiento, libroFavorito ]
+
+console.log(lista);
