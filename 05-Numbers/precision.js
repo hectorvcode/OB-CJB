d6d37095ee8131f1debdb8c46e73a59e348299ae
HectorV <hectorfabiovasquez@gmail.com>
2022-09-22 20:22:25 -0500

    Added Numbersy precisión en JS

---
+++ 05-Numbers/precision.js
@@ -0,0 +1,15 @@
+// Sesión 5 - Numbers
+// Declaración de variables numéricas (Enteros y con Decimales)
+
+let a = 5;
+console.log(a);
+
+let b = 0.1;
+console.log(b);
+
+// Precisión
+let c = 0.2;
+console.log(b + c);
+
+//truco para obtener valor "real"
+console.log(Math.round((0.1 + 0.2)*100)/100); 
