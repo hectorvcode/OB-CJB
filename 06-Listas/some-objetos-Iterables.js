8b016c184d1dc1c176b937e44e2be7a793e007ca
HectorV <hectorfabiovasquez@gmail.com>
2022-09-24 10:22:03 -0500

    Added Identificar si existe un valor en un array y objetos iterables

---
+++ 06-Listas/some-objetos-Iterables.js
@@ -0,0 +1,44 @@
+// .some() pasa true en caso que al menos uno de los valores cumpla la condición
+const array = [3, 7, 2, 4, 7, 9, 42, 35, 7865, 23, -2];
+
+const res = array.some(valor => valor < 0);
+console.log(res);
+
+const existe =  array.some(valor => valor ===9);
+console.log(existe);
+
+const listaObjetos = [
+    { nombre: "Leire", edad: 35 },
+    { nombre: "Gorka", edad: 34 },
+    { nombre: "Miguel", edad: 28 },
+    { nombre: "Lucia", edad: 3 },
+    { nombre: "Amaia", edad: 29 },
+]
+
+const existeMiguel = listaObjetos.some(persona => persona.nombre === "Miguel");
+console.log(existeMiguel);
+
+const existeJuan = listaObjetos.some(persona => persona.nombre === "Juan");
+console.log(existeJuan);
+
+
+// Como obtener una lista a partir de un objeto iterable
+const str = "Hola soy Gorka"
+console.log(str[0]);
+
+const ar_str = Array.from(str);
+console.log(ar_str);
+
+const set = new Set([2,3,"hola",4]);
+console.log(set);
+
+const ar_set = Array.from(set);
+console.log(ar_set);
+
+
+// Obtener un iterable de todos los indices del array
+const keys = array.keys();
+console.log(keys);
+
+const ar_keys = Array.from(keys);
+console.log(ar_keys);
