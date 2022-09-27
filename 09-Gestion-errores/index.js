bdfeb0488091fbb9bdbb804499566874cb37dab7
HectorV <hectorfabiovasquez@gmail.com>
2022-09-27 04:51:28 -0500

    Added Gestion de logs en NodeJS

--- 09-Gestion-errores/index.js
+++ 09-Gestion-errores/index.js
@@ -1,18 +1,4 @@
-const winston = require('winston');
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
+const logger = require('./logger')
 
 //logger.log("Hola estoy saliendo por pantalla");
 logger.info("Mensaje informativo");
