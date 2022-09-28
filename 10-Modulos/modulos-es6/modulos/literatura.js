6790e70419889185122417b23a5c627f1e589add
HectorV <hectorfabiovasquez@gmail.com>
2022-09-28 08:35:03 -0500

    Added Usando módulos con ES6

---
+++ 10-Modulos/modulos-es6/modulos/literatura.js
@@ -0,0 +1,8 @@
+const getAutor = () => {
+    return "Miguel de Cervantes"
+}
+
+export const libro = "Don Quijote de la Mancha"
+
+// Solo se puede hacer una exportación por defecto
+export default getAutor;
