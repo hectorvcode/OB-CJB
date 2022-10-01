38e5d4b3627bd563ad8ce89190ecabe366cef2b0
HectorV <hectorfabiovasquez@gmail.com>
2022-10-01 06:38:13 -0500

    Added Herencia y Polimorfismo

---
+++ 11-POO/05-herencia.js
@@ -0,0 +1,32 @@
+// Inheritance - Herencia
+class Persona {
+    _nombre;
+    _edad;
+    constructor(nombre, edad){
+        this._nombre = nombre;
+        this._edad = edad;
+    }
+
+    saludo(){
+        console.log(`Hola, mi nombre es ${this._nombre}, tengo ${this._edad} años`);
+    }
+}
+
+class Desarrollador extends Persona {
+    constructor(nombre, edad, lenguaje){
+        super(nombre, edad);
+        this.lenguaje = lenguaje;
+    }
+
+    // Overwrite
+    saludo() {
+        super.saludo();
+        console.log(`Y soy desarrollador de ${this.lenguaje}`);
+    }
+}
+
+const nuevoDev =  new Desarrollador("Forka", 34, "JavaScript");
+console.log(nuevoDev);
+nuevoDev.saludo();
+
+// Polimorfismo -> varias formas de las clases
