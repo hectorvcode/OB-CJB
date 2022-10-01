62468a3ace0891f297ab34f495721ad8eb6a9b28
HectorV <hectorfabiovasquez@gmail.com>
2022-10-01 16:03:06 -0500

    Added some for loop exercises

---
+++ 12-Depuracion/02-ejercicio.js
@@ -0,0 +1,13 @@
+/**
+ * Crea un nuevo fichero JS que contenga las siguientes líneas
+- Una función que admita un parámetro "num", y devuelva una lista con esa cantidad de números de la secuencia de Fibonacci (Por ejemplo: num = 6 => Resultado [1, 1, 2, 3, 5, 8])
+- Ejecuta la depuración de VSCode para visualizar la ejecución de la función
+ */
+
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
