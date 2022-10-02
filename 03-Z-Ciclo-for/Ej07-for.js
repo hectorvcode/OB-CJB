6383ee91acb5f8bd6d963a14aadd4d501d2770e5
HectorV <hectorfabiovasquez@gmail.com>
2022-10-02 13:07:08 -0500

    Added Exercise 07 and Exercise 08 - For Loop

---
+++ 03-Z-Ciclo-for/Ej07-for.js
@@ -0,0 +1,28 @@
+// #1 — Print the numbers from 0 to 15
+
+for(let i = 0; i <= 15; i++){
+    console.log(i);
+}
+
+// #2 — Print the numbers from 12 to 24
+
+for(let i = 12; i <= 24; i++){
+    console.log(i);
+}
+
+// #3 — Print the ODD numbers from 7 to 31
+
+for(let i = 7; i <= 31; i++){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
+
+// #4 — Print the EVEN numbers from 10 to -20
+
+for(let i = 10; i >= -20; i--){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
+
