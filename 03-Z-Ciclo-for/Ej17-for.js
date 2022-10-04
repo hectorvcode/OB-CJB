6b81dce213670a540298554c056f4bcba17781eb
HectorV <hectorfabiovasquez@gmail.com>
2022-10-04 08:50:09 -0500

    Added Exercise 17 For Loop

---
+++ 03-Z-Ciclo-for/Ej17-for.js
@@ -0,0 +1,11 @@
+// Exercise 8)
+// Write a program that will allow someone to guess a four digit pin exactly 4
+// times. If the user guesses the number correctly. It prints “That was
+// correct!” Otherwise it will print “Sorry that was wrong.” Program stops after 
+//the 4th attempt of if they got it right.
+let pin = 0704;
+console.log("Please make your guess");
+
+for(let i = 1; i <= 4; i++){
+ console.log(i);
+}
