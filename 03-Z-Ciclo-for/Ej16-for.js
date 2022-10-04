aa34e1db09ec92af0709a81930126f49308299ec
HectorV <hectorfabiovasquez@gmail.com>
2022-10-04 08:44:03 -0500

    Added Exercise 16 For Loop

---
+++ 03-Z-Ciclo-for/Ej16-for.js
@@ -0,0 +1,9 @@
+// Exercise 7) Given a number n Calculate the factorial of the number
+
+let n2 = 10; //  4 * 3 * 2 * 1 = 24
+let multiplicacion = 1;
+
+for(let i = 1; i <= n2; i++){
+    multiplicacion = multiplicacion * i
+    console.log(multiplicacion);
+}
