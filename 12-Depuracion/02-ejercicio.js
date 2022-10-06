274561421015d4bbd6a31a4c529b3c79071d3212
HectorV <hectorfabiovasquez@gmail.com>
2022-10-06 09:15:03 -0500

    Added Ejercicio Section 12

--- 12-Depuracion/02-ejercicio.js
+++ 12-Depuracion/02-ejercicio.js
@@ -4,10 +4,28 @@
 - Ejecuta la depuración de VSCode para visualizar la ejecución de la función
  */
 
-for(let i = 1; i <= 10; i++){
-    console.log(i);
+/* function Fibo(number){
+    if(number <= 1){
+        return number;
+    }
+    const result = [0, 1];
+    for(let i = 2; i <= number; i++){
+        result[i] = result[i - 2] + result[i - 1]
+    }
+    return result[result.length - 1]
+}
+console.log(Fibo(7)); */
+
+
+function Fibo(numero){
+    let n1 = 0, n2 = 1, nextNum, i;
+    
+    for(i = 0; i < numero; i++){
+        nextNum = n1 + n2;
+        n1 = n2;
+        n2 = nextNum;
+    }
+    return n1;
 }
 
-for(let i = 1; i <= 10; i++){
-    console.log(i);
-}
+console.log(Fibo(6));
