29faa0ad1e846e124dcf2f9663f7266832061ffe
HectorV <hectorfabiovasquez@gmail.com>
2022-10-06 08:27:40 -0500

    Added Exercise 02 While Loop

--- 03-Z-Ciclo-while/Ej01-while.js
+++ 03-Z-Ciclo-while/Ej01-while.js
@@ -1,4 +1,4 @@
-// Create a loop that runs as long as i is less than 10
+// Exercise 01 -Create a loop that runs as long as i is less than 10
 
 let i = 0;
 
@@ -7,3 +7,11 @@ while(i < 10){
     console.log(i);
 }
 
+// Exercise 2 - Create a loop that runs as long as i is less than 10, but increase i with 2 each time.
+
+let j = 0;
+
+while(j < 10){
+    j += 2;
+    console.log(j);
+}
