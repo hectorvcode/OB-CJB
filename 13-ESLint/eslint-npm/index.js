96614e139770d6033f56178cd5dc2fd16f2e7ee3
HectorV <hectorfabiovasquez@gmail.com>
2022-10-08 06:48:55 -0500

    Added Reglas temporales y scripts para ejecución de ESLint en nuestro código

--- 13-ESLint/eslint-npm/index.js
+++ 13-ESLint/eslint-npm/index.js
@@ -1,3 +1,20 @@
 const nombre = "Gorka"
 
-const persona2 = "Maria"
+//En esta linea quiero tener comillas simples
+
+/* eslint-disable */
+const persona2 = 'Maria'
+
+/* eslint-enable */
+const nuevoString = 'Esto es un nuevo string'; //eslint-disable-line
+
+/* eslint-disable-next-line indent */
+  const string2 = "Más strings"
+
+console.log("Hola")
+
+const persona3 = "Maria"
+
+const nombre3 = "Julian" 
+
+console.log(4)
