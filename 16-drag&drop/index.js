01e3b9b1874f66cf2f23a4bb8460e6cbcbe3f74a
HectorV <hectorfabiovasquez@gmail.com>
2022-10-08 19:45:16 -0500

    Added Drag and Drop con JavaScript

---
+++ 16-drag&drop/index.js
@@ -0,0 +1,34 @@
+const parrafos  = document.querySelectorAll(".parrafo");
+const secciones = document.querySelectorAll(".seccion");
+
+parrafos.forEach(parrafo => {
+    parrafo.addEventListener("dragstart", event => {
+        //console.log("click");
+        console.log("Estoy arrastrando el párrafo: " + parrafo.innerText);
+        parrafo.classList.add("dragging");
+        event.dataTransfer.setData("id", parrafo.id);
+        const elemento_fantasma = document.querySelector(".imagen-fantasma");
+        event.dataTransfer.setDragImage(elemento_fantasma, 0, 0);
+    })
+    parrafo.addEventListener("dragend", () => {
+        //console.log("click");
+        //console.log("He terminado de arrastrar");
+        parrafo.classList.remove("dragging");
+    })
+})
+
+secciones.forEach(seccion => {
+    seccion.addEventListener("dragover", event => {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = "move"; //copy por default
+        //console.log("Drag Over");
+    })
+
+    seccion.addEventListener("drop", event => {
+        console.log("Drop");
+        const id_parrafo = event.dataTransfer.getData("id");
+        //console.log(id_parrafo);
+        const parrafo = document.getElementById(id_parrafo);
+        seccion.appendChild(parrafo);
+    })
+})
