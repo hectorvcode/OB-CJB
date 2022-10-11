50bc1c582d5391cae689cfc4162fd0faf9661c3d
HectorV <hectorfabiovasquez@gmail.com>
2022-10-10 22:15:38 -0500

    Added Exercise 05 Ej02-while.js

--- 03-Z-Ciclo-while/Ej02-while.js
+++ 03-Z-Ciclo-while/Ej02-while.js
@@ -42,5 +42,26 @@ while(l >= -20){
 }
 
 
+/**
+#5 — Iterate through all numbers from 1 to 45. Print the following:
 
+For multiples of 3 print “Fizz”
+For multiples of 5 print “Buzz”
+For multiples of 3 and 5 print “FizzBuzz”
+ */
 
+console.log("FizzBuzz Exercise");
+let n = 1;
+
+while(n <= 45){
+    if(n % 3 === 0 && n % 5 === 0){
+        console.log("FizzBuzz");
+    } else if(n % 3 === 0) {
+        console.log("Fizz");
+    } else if(n % 5 === 0){
+        console.log("Buzz");
+    } else {
+        console.log(n);
+    }
+    n++
+}
