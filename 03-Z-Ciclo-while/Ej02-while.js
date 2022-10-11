22fb3ef02cfb8e532fd2a35ef931b80a86d41ca7
HectorV <hectorfabiovasquez@gmail.com>
2022-10-10 21:53:23 -0500

    Added Exercise 01, 02, 03 While loops

---
+++ 03-Z-Ciclo-while/Ej02-while.js
@@ -0,0 +1,32 @@
+// #1 — Print the numbers from 0 to 15
+console.log("Numbers from 0 to 15");
+let i = 0;
+
+while(i <= 15){
+    console.log(i);
+    i++;
+}
+
+
+//#2 — Print the numbers from 12 to 24
+console.log("Numbers from 12 to 24");
+let j = 12;
+
+while(j <= 24){
+    console.log(j);
+    j++;
+}
+
+
+//#3 — Print the ODD numbers from 7 to 31
+console.log("Print the ODD numbers from 7 to 31");
+let k = 7;
+
+while(k <= 31){
+    if(k % 2 !== 0){
+        console.log(k);
+    }
+    k++;
+}
+
+
