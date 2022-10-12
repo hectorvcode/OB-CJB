c8e7b0259426d9d573694c0bc2b1cfa50aca441d
HectorV <hectorfabiovasquez@gmail.com>
2022-10-11 22:48:07 -0500

    Added Exercise 7 - While Loops

---
+++ 03-Z-Ciclo-while/Ej07-while.js
@@ -0,0 +1,14 @@
+/**
+Exercise 7)
+Given a number n Calculate the factorial of the number
+*/
+
+let n2 = 6; //  4 * 3 * 2 * 1 = 24
+
+let i = 1;
+let multi = 1;
+while(i <= n2){
+    multi = multi * i
+    i++;
+}
+console.log(multi);
