65698113d27dcb8aba0d27d880891206f7c4326d
HectorV <hectorfabiovasquez@gmail.com>
2022-10-12 20:08:25 -0500

    Added Exercise 2 - Switch

---
+++ 03-Z-Ciclo-Switch/Ej02-switch.js
@@ -0,0 +1,14 @@
+/**
+Create a switch statement that will alert "Hello" if fruits is "banana", and "Welcome" if fruits is "apple".
+ */
+
+let fruits = "apple";
+
+switch(fruits){
+    case "banana":
+        console.log("Hello");
+        break;
+    case "apple":
+        console.log("Welcome");
+        break;
+}
