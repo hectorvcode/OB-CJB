065961683c5e36988d54a63a06fcf3c26560a9f0
HectorV <hectorfabiovasquez@gmail.com>
2022-10-12 20:52:47 -0500

    Added Exercise 01 - String Methods

---
+++ 04-Strings/04-Z-Strings-Methods/Ej01-strings.js
@@ -0,0 +1,59 @@
+/**
+1. Write a JavaScript function to check whether an `input` is a string or not.
+Test Data :
+console.log(is_string('w3resource'));
+true
+console.log(is_string([1, 2, 4, 0]));
+false
+ */
+console.log("Is a string?");
+function is_string(string){
+    if(typeof string === "string"){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(is_string([1, 2, 4, 0]));
+
+/**
+2. Write a JavaScript function to check whether a string is blank or not.
+Test Data :
+console.log(is_Blank(''));
+console.log(is_Blank('abc'));
+true
+false
+ */
+console.log("Is blank?");
+function is_Blank(string1){
+    if(string1.length === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(is_Blank(''));
+
+/**
+3. Write a JavaScript function to split a string and convert it into an array of words. Go to the editor
+Test Data :
+console.log(string_to_array("Robin Singh"));
+["Robin", "Singh"]
+ */
+console.log("Split String");
+function string_to_array(string2){
+    return string2.split(" ");
+}
+console.log(string_to_array("Robin Singh"));
+
+/**
+4. Write a JavaScript function to extract a specified number of characters from a string.
+Test Data :
+console.log(truncate_string("Robin Singh",4));
+"Robi"
+ */
+console.log("Extract Characters");
+function truncate_string(string3, numChars){
+    return string3.substring(0, numChars);
+}
+console.log(truncate_string("Robin Singh", 4));
