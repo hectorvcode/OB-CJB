03ed5a53af40ce620eee6cc5f9afc4f4d54c0d7f
HectorV <hectorfabiovasquez@gmail.com>
2022-10-12 19:57:00 -0500

    Added Exercise 1 - Switch

---
+++ 03-Z-Ciclo-Switch/Ej01-switch.js
@@ -0,0 +1,32 @@
+/**
+The getDay() method returns the weekday as a number between 0 and 6.
+(Sunday=0, Monday=1, Tuesday=2 ..)
+ */
+
+let day;
+
+switch(new Date().getDay()){
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+}
+
+console.log("Today is " + day);
