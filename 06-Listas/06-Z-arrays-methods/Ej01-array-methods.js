dcd0dda2929ec0e3aa20b5320ecb5a5750819b61
HectorV <hectorfabiovasquez@gmail.com>
2022-10-15 07:50:43 -0500

    Added Exercise 02 - Ej01-array-methods.js

--- 06-Listas/06-Z-arrays-methods/Ej01-array-methods.js
+++ 06-Listas/06-Z-arrays-methods/Ej01-array-methods.js
@@ -6,9 +6,25 @@ console.log(is_array([1, 2, 4, 0]));
 false
 true
  */
+console.log("Is this an array?");
 function is_array(arr1){
     return Array.isArray(arr1);
 }
 console.log(is_array('w3resource'));
 console.log(is_array([1, 2, 4, 0]));
+
+/**
+2. Write a JavaScript function to clone an array. Go to the editor
+Test Data :
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+[1, 2, 4, 0]
+[1, 2, [4, 0]]
+ */
+console.log("Clonning an array");
+function array_Clone(arr2){
+    return [...arr2]
+}
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
 
