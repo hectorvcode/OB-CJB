01c574b74fd34d7d4a8a45716618e855e708325e
HectorV <hectorfabiovasquez@gmail.com>
2022-10-17 19:55:54 -0500

    Added Exercise 03 - Ej01-array-methods.js

--- 06-Listas/06-Z-arrays-methods/Ej01-array-methods.js
+++ 06-Listas/06-Z-arrays-methods/Ej01-array-methods.js
@@ -1,30 +1,38 @@
 /**
-1. Write a JavaScript function to check whether an `input` is an array or not. Go to the editor
-Test Data :
-console.log(is_array('w3resource'));
-console.log(is_array([1, 2, 4, 0]));
-false
-true
+multiply each element by 2 while not changing the original array
  */
-console.log("Is this an array?");
-function is_array(arr1){
-    return Array.isArray(arr1);
+console.log("Multiplying by");
+let arrOne = [32, 45, 63, 36, 24, 11]
+
+const multTwo = (num) => {
+    return num * 2;
 }
-console.log(is_array('w3resource'));
-console.log(is_array([1, 2, 4, 0]));
+
+let arrTwo = arrOne.map(multTwo);
+console.log(arrTwo);
+
 
 /**
-2. Write a JavaScript function to clone an array. Go to the editor
-Test Data :
-console.log(array_Clone([1, 2, 4, 0]));
-console.log(array_Clone([1, 2, [4, 0]]));
-[1, 2, 4, 0]
-[1, 2, [4, 0]]
+Suppose you have an array arrName and that array stores a 
+bunch of numbers. Now, you would like to see what numbers 
+can be divided by 3 and make a separate array from them.
  */
-console.log("Clonning an array");
-function array_Clone(arr2){
-    return [...arr2]
+console.log("Divided by 3");
+let arrNum = [15, 39, 20, 32, 30, 45, 22];
+function divByThree(num){
+    return num % 3 == 0;
 }
-console.log(array_Clone([1, 2, 4, 0]));
-console.log(array_Clone([1, 2, [4, 0]]));
+let arrNewNum = arrNum.filter(divByThree);
+console.log(arrNewNum);
+
+/**
+Sum of all elements of an array
+ */
+console.log("Now with reduce");
+let arrNum1 = [15, 39, 20, 32, 30, 45, 22];
+function sumOfElements(num, ind){
+    return num + ind;
+}
+let arrNum2 = arrNum1.reduce(sumOfElements);
+console.log(arrNum2);
 
