35e2d116e21dc0df8f85809ff97bb9995ccecec6
HectorV <hectorfabiovasquez@gmail.com>
2022-10-19 22:00:53 -0500

    Added Convert numbers to strings - Ej03-array-methods.js

--- 06-Listas/06-Z-arrays-methods/Ej03-array-methods.js
+++ 06-Listas/06-Z-arrays-methods/Ej03-array-methods.js
@@ -1,9 +1,15 @@
-/**
-1) Make an array of numbers that are doubles of the first array
- */
+
+//1) Make an array of numbers that are doubles of the first array
 
 function doubleNumbers(arr){
     return arr.map(num => num * 2);
 }
-
 console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
+
+
+//2) Take an array of numbers and make them strings
+
+function stringItUp(arr){
+    return arr.map(num => num.toString());
+}
+console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
