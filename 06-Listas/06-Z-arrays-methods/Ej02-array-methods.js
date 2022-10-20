48ce78ee04eb518bffa346f6995d2d1f33e2c616
HectorV <hectorfabiovasquez@gmail.com>
2022-10-19 21:31:02 -0500

    Added abbreviate everything - Ej02-array-methods.js

--- 06-Listas/06-Z-arrays-methods/Ej02-array-methods.js
+++ 06-Listas/06-Z-arrays-methods/Ej02-array-methods.js
@@ -97,3 +97,31 @@ const ages = input5.map(person => person.age);
 const result5 = [Math.min(...ages), Math.max(...ages), Math.max(...ages) - Math.min(...ages)];
 
 console.log(result5);
+
+
+/**
+Numeronyms
+Devs like to abbreviate everything: k8s means Kubernetes, a11y means accessibility, l10n means localization. 
+You get the Dev numeronyms by taking the first and the last letter and counting the number of letters in between.
+Words that have less than 4 letters aren't abbreviated, because that would just be odd. 
+The input is a sentence, and you should abbreviate every word that is 4 letters long or longer. 
+There won't be any punctuation in the sentence. g2d l2k e6e
+
+Helpful links
+Array.prototype.map()
+String.prototype.split()
+String.prototype.join()
+
+ans: 'E3y d7r l3s to mix k8s and j8t'
+ */
+
+const input6 = 'Every developer likes to mix kubernetes and javascript';
+
+const createNumeronym = (word) => word[0] + (word.length - 2) + word[word.length - 1];
+
+let ans = 
+input6
+.split(" ")
+.map(word => word.length >= 4 ? createNumeronym(word): word)
+.join(" ");
+console.log(ans);
