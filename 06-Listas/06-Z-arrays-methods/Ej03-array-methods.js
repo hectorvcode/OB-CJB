d03d3e12f2b9f56e5d9cb10c56396bc2897c419d
HectorV <hectorfabiovasquez@gmail.com>
2022-10-22 08:04:09 -0500

    Added How many times each pet appears in the array - Ej03-array-methods.js

--- 06-Listas/06-Z-arrays-methods/Ej03-array-methods.js
+++ 06-Listas/06-Z-arrays-methods/Ej03-array-methods.js
@@ -70,3 +70,16 @@ const sum = numbers.reduce(function(result, item){
 }, 0);
 console.log(sum);
 
+// Transform an array of strings into a single object that shows how many times each string appears in the array
+
+let pets = ['dog', 'chicken', 'cat', 'dog', 'chicken', 'chicken', 'rabbit'];
+
+let petCounts = pets.reduce(function(obj, pet){
+    if(!obj[pet]){
+        obj[pet] = 1;
+    } else {
+        obj[pet]++;
+    }
+    return obj;
+}, {});
+console.log(petCounts);
